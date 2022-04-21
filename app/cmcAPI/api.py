414eff4c7621a8fb636726718c571e893de6a0a4
RAMZI <ramzi446@hotmail.com>
2022-04-21 15:37:50 +0200

    appel a l'api

---
+++ app/cmcAPI/api.py
@@ -0,0 +1,26 @@
+import os
+import json
+import requests
+class Cmc:
+    def __init__(self):
+        self.base_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+        self.parameters = {
+            'start': '1',
+            'limit': '100',
+            'convert': 'USD'
+        }
+        self.headers = {
+            'Accepts': 'application/json',
+            'X-CMC_PRO_API_KEY': os.environ.get('CMC_API_KEY')
+        }
+    def get_all(self):
+        response = requests.get(self.base_url, headers=self.headers, params=self.parameters)
+        response_json = response.json()
+        return response_json['data']
+
+    def get_by_slug(self, slug):
+        self.base_url = "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest"
+        self.parameters ={'slug': slug}
+        response = requests.get("https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest", headers=self.headers, params=self.parameters)
+        response_json = response.json()
+        return response_json['data']
--- app/views/home.py
+++ app/views/home.py
@@ -1,6 +1,26 @@
 from flask import Blueprint, render_template
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+from ..cmcAPI.api import Cmc
 home = Blueprint('home', __name__)
 
 @home.route('/')
 def index():
-    return render_template('home/index.html')
+    cmc = Cmc()
+    all_data = cmc.get_all()
+    return render_template('home/index.html', data=all_data)
+"""    session = Session()
+    session.headers.update(headers)
+
+    try:
+        response = session.get(url_covert, params=parameters)
+        data = json.loads(response.text)
+        json_string = json.dumps(data)
+        with open('data.json', 'w') as outfile:
+            outfile.write(json_string)
+        datas = data
+        print(datas)
+        return render_template('home/index.html', data=data)
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)"""
